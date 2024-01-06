86602bd64e3a6ded88bac231db5c3a8f17a4f056
HRT04 <e215707@ie.u-ryukyu.ac.jp>
2024-01-06 23:55:02 +0900

    commit2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup>
+<!-- <script setup>
 import { ModelViewer } from "@reidjs/vue3d";
 </script>
 
 <template>
   <ModelViewer src="./rocket.glb" auto-rotate camera-controls></ModelViewer>
-</template>
+</template> -->
